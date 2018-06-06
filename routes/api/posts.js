ce899b38b83784fa354a1da22cd396a220981ba6
Jarrod Medrano <jarrodmedrano@Jarrods-MacBook-Pro.local>
2018-06-05 18:51:43 -0600

    adding password checking

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // @desc    Tests post route
 // @access  Public
 router.get('/test', (req, res) => res.json({
-    msg: "Post Works"
+    msg: "Posts Works"
 }));
 
 module.exports = router;
