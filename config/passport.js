619b9812fef9503dd60294839dd398d1d02f5cb4
Jarrod Medrano <jarrodmedrano@Jarrods-MacBook-Pro.local>
2018-06-06 18:46:01 -0600

    adding comments

--- config/passport.js
+++ config/passport.js
@@ -1,18 +1,20 @@
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
+//comes from Users model
 const User = mongoose.model("users");
+//validate request
 const keys = require("../config/keys");
 const passport = require("passport");
 
-var opts = {};
+const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
-      console.log(jwt_payload);
+      console.log(jwt_payload.id);
     })
   );
 };
