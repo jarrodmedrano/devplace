5a1c65a0b4c94874386ea0eb5853447a732cf3c2
Jarrod Medrano <jarrodmedrano@Jarrods-MacBook-Pro.local>
2018-06-16 13:22:45 -0600

    post model

---
+++ models/Post.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Create Scema
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      },
+      text: {
+        type: String,
+        required: true
+      }
+    }
+  ]
+});
+
+module.exports = Post = mongoose.model("post", PostSchema);
